aa8843367cb1a5dd46b41163070a741417f30659
Moritz <mosum@google.com>
2025-12-30 16:52:21 +0100

    Use package:font_asset

--- hook/build.dart
+++ hook/build.dart
@@ -1,15 +1,14 @@
-import 'package:font_asset_test/font_asset.dart';
+import 'package:font_asset/font_asset.dart';
 import 'package:hooks/hooks.dart';
 
 void main(List<String> arguments) {
   build(arguments, (input, output) async {
-    output.assets.addEncodedAsset(
+    output.assets.fonts.add(
       FontAsset(
         file: input.packageRoot.resolve('fonts/BBHBartle-Regular.ttf'),
-        // weight: 700,
         fontFamily: 'BBHBartle',
         package: input.packageName,
-      ).encode(),
+      ),
     );
   });
 }
